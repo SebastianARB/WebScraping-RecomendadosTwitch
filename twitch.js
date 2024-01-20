5573f1cd037ff7c1c6e491ff867143eac9a6be4a
Sebastian Arbona <45405577+SebastianARB@users.noreply.github.com>
2024-01-20 18:45:58 -0300

    Optimización de codigo

--- twitch.js
+++ twitch.js
@@ -1,42 +1,46 @@
 const puppeteer = require("puppeteer");
-const fs = require("fs").promises; // Importa el módulo fs para trabajar con archivos con promesas
+const fs = require("fs").promises;
+
+const SELECTORS = {
+  CHANNEL: '[data-test-selector="recommended-channel"]',
+  NAME: '[data-a-target="side-nav-title"]',
+  TITLE: '[data-a-target="side-nav-game-title"]',
+  VIEWERS: '[class="CoreText-sc-1txzju1-0 gWcDEo"]',
+  IMAGE: '[class="InjectLayout-sc-1i43xsx-0 cXFDOs tw-image tw-image-avatar"]',
+};
 
 (async () => {
   try {
-    // Iniciar el navegador
     const browser = await puppeteer.launch({
-      headless: false, // Muestra el navegador en pantalla
-      defaultViewport: null, // Usa la configuración de vista predeterminada
+      headless: false,
+      defaultViewport: null,
     });
 
-    // Abrir una nueva página
     const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 }); // Puedes ajustar el ancho y alto según tus necesidades
+    await page.setViewport({ width: 1920, height: 1080 });
     await page.goto("https://www.twitch.tv/");
 
-    // Recopilar datos
-    const recomendados = await page.evaluate(() => {
-      const items = document.querySelectorAll('[data-test-selector="recommended-channel"]');
+    const recomendados = await page.evaluate((selectors) => {
+      const items = document.querySelectorAll(selectors.CHANNEL);
       const arr = [];
 
       for (let item of items) {
         const canal = {};
-        canal.name = item.querySelector('[data-a-target="side-nav-title"]').innerText;
-        canal.title = item.querySelector('[class="Layout-sc-1xcs6mc-0 eMtMuE side-nav-card__metadata"]').innerText;
-        canal.viewers = item.querySelector('[class="CoreText-sc-1txzju1-0 dSWFc"]').innerText;
-        canal.image = item.querySelector('[class="InjectLayout-sc-1i43xsx-0 gljEcG tw-image tw-image-avatar"]').src;
-    
+        canal.name = item.querySelector(selectors.NAME).innerText;
+        canal.title = item.querySelector(selectors.TITLE).innerText;
+        canal.viewers = item.querySelector(selectors.VIEWERS).innerText;
+
+        const imageElement = item.querySelector(selectors.IMAGE);
+        canal.image = imageElement ? imageElement.src : null;
 
         arr.push(canal);
       }
       return arr;
-    });
+    }, SELECTORS);
 
-    // Cerrar el navegador
     await browser.close();
 
-    // Guardar los datos en un archivo JSON
-    const jsonData = JSON.stringify(recomendados, null, 2); // Formatea con sangría
+    const jsonData = JSON.stringify(recomendados, null, 2);
     await fs.writeFile('recomendados.json', jsonData);
     console.log('Datos guardados en recomendados.json');
   } catch (error) {
